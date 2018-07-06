64b6ccd62ddbc7d1654b6dc47a37a790ad73a84d
Tyler Van Hoomissen <tvanh@atlassian.com>
2018-07-06 10:17:10 -0700

    Remove explicit words

--- src/lib/aussie-ipsum.js
+++ src/lib/aussie-ipsum.js
@@ -1,160 +1,380 @@
 export const aussieIpsum = [
   {
-    word: "A different kettle of fish",
-    meaning: "A different thing altogther."
+    word: 'aggro',
+    meaning: 'Aggressive.',
   },
   {
-    word: "Arvo",
-    meaning: "Afternoon."
+    word: 'arvo',
+    meaning: 'Afternoon.',
   },
   {
-    word: "Bloke",
-    meaning: "A dude."
+    word: 'bags out',
+    meaning: 'Disses.',
   },
   {
-    word: "Bogan",
-    meaning:
-      "A person who takes little pride in his appearance, spends his days slacking and drinking beer."
+    word: 'becky',
+    meaning: 'White girl.',
   },
   {
-    word: "Ciggy butt brain",
-    meaning: "When you got ciggy butts all up in your brain.",
-    link: "https://www.youtube.com/watch?v=vmL72sgVdAQ"
+    word: 'bickie',
+    meaning: 'Biscuit.',
   },
   {
-    word: "Heaps",
-    meaning: "A lot of, mate."
+    word: 'bikies',
+    meaning: 'Leather suit.',
   },
   {
-    word: "Keen",
-    meaning: "Showing eagerness or enthusiasm."
+    word: 'bingle',
+    meaning: 'Fender bender.',
   },
   {
-    word: "Mate",
-    meaning: "A dude."
+    word: 'bloke',
+    meaning: 'A dude.',
   },
   {
-    word: "Petrol",
-    meaning: "Gasoline."
+    word: 'bludger',
+    meaning: 'Slacker.',
   },
   {
-    word: "Smoko",
-    meaning: "A smoke break.",
-    link: "https://www.youtube.com/watch?v=j58V2vC9EPc"
+    word: 'bogan',
+    meaning: 'Red neck.',
   },
   {
-    word: "Spicy",
-    meaning: "Interesting, controversial and exciting."
+    word: 'bottle-o',
+    meaning: 'Bottle-.',
   },
   {
-    word: "Stickybeak",
-    meaning: "An inquisitive and prying person."
+    word: 'breky',
+    meaning: 'Breakfast.',
   },
   {
-    word: "Tomato sauce",
-    meaning: "Ketchup."
+    word: 'brisvegas',
+    meaning: 'Brisvega.',
   },
   {
-    word: "Vegemite",
-    meaning: "Spread thin to avoid regret."
+    word: 'budgie smuggler',
+    meaning: 'Speedo.',
   },
-  { word: "Cooee", description: "something is close" },
-  { word: "Heaps", description: "Heap" },
-  { word: "Bogan", description: "red neck" },
-  { word: "Punch a cone", description: "smoke out of a bong" },
   {
-    word: "Built like a brick shithouse",
-    description: "someone who is jacked"
+    word: 'cabsav',
+    meaning: 'Red wine.',
   },
-  { word: "On the cold stuff", description: "doing meth" },
-  { word: "Schooner", description: "measurement of beer" },
-  { word: "Galoot", description: "same as below" },
-  { word: "Gla", description: "dickhead, nice" },
-  { word: "Goon sack", description: "swing it around" },
-  { word: "Bingle", description: "fender bender" },
-  { word: "Bludger", description: "slacker" },
-  { word: "Compo", description: "compensation" },
-  { word: "Maggot", description: "disgustingly drunk" },
-  { word: "Tactical vom", description: "Tactical vom" },
-  { word: "Jumbuck", description: "sheep" },
-  { word: "Bags out", description: "disses" },
-  { word: "Jumper", description: "hoodie" },
-  { word: "Derro", description: "derelict" },
-  { word: "Minge", description: "Ming" },
-  { word: "Clunge", description: "vag" },
-  { word: "Connie, Jonny", description: "condom" },
-  { word: "Can't be stuffed", description: "can't be fucked with" },
-  { word: "Fuckwit", description: "dickhead" },
-  { word: "Budgie smuggler", description: "speedo" },
-  { word: "Whoop whoop", description: "too far" },
-  { word: "Daggy", description: "dorky" },
-  { word: "lollies", description: "candy" },
-  { word: "Get fucked", description: "fuck off" },
-  { word: "Pingas", description: "pills, E" },
-  { word: "Lift", description: "elevator" },
-  { word: "Durries", description: "already smoked cigarette/ new cigarette" },
-  { word: "Wagga Wagga", description: "place in central" },
-  { word: "Yah nah / nah yah", description: "Yah nah / nah ya" },
-  { word: "Bikies", description: "leather suit" },
-  { word: "S'avro", description: "this afternoon" },
-  { word: "Tinny", description: "can" },
-  { word: "Exxy", description: "expensive" },
-  { word: "Cuppa", description: "Cupp" },
-  { word: "He's loose", description: "he's drunk" },
-  { word: "He's off his face", description: "he's drunk" },
-  { word: "Wanna root?", description: "Would you like to fuck?" },
-  { word: "Servo", description: "Serv" },
-  { word: "Bottle-o", description: "Bottle-" },
-  { word: "Sikkunt", description: "great bloke / girl" },
-  { word: "Becky", description: "white girl" },
-  { word: "GC", description: "good cunt" },
-  { word: "Straya", description: "Stray" },
-  { word: "You beaut", description: "something is good" },
-  { word: "Bickie", description: "biscuit" },
-  { word: "Breky", description: "breakfast" },
-  { word: "Brisvegas", description: "Brisvega" },
-  { word: "CabSav", description: "red wine" },
-  { word: "Karked", description: "something died" },
-  { word: "Chuck a sikkie", description: "call in sick" },
-  { word: "Dead set", description: "true" },
-  { word: "Dinkie Die", description: "really true" },
-  { word: "Off her tits", description: "she's drunk" },
-  { word: "Drongo", description: "idiot" },
-  { word: "dunnie", description: "the loo" },
-  { word: "Fair dinkum", description: "genuine" },
-  { word: "Top bloke", description: "Top bloke" },
-  { word: "coasties", description: "person living on coast" },
-  { word: "grog", description: "booze" },
-  { word: "hoolie doolie", description: "in surprised! Good heavens!" },
-  { word: "hoon", description: "fuck around in car" },
-  { word: "jug", description: "kettle or jug" },
-  { word: "larrikin", description: "class clown" },
-  { word: "mates rates", description: "friend discount" },
-  { word: "postie", description: "postman" },
-  { word: "prezzy", description: "present" },
-  { word: "rego", description: "vehicle registration" },
-  { word: "relo", description: "relatives" },
-  { word: "ridgy-didge", description: "something is good" },
-  { word: "whinge", description: "rant" },
-  { word: "true blue", description: "really patriotic" },
-  { word: "sticky beak", description: "being nosy" },
-  { word: "Cactus", description: "dead" },
-  { word: "or nothing but buckleys", description: "you've got no chance" },
-  { word: "Shit all", description: "no chance" },
-  { word: "Gone walk-about", description: "it's lost" },
-  { word: "streuth", description: "oh shit!" },
-  { word: "onya", description: "good on you" },
-  { word: "Chuck a block", description: "its full" },
-  { word: "Piece of piss", description: "it's so easy" },
-  { word: "Docket", description: "receipt" },
-  { word: "gridiron", description: "American football" },
-  { word: "mueslie", description: "granola" },
-  { word: "Saltana", description: "raisin" },
-  { word: "Rocket", description: "arugula" },
-  { word: "Tap and Gap", description: "one night stand" },
-  { word: "thongs", description: "flip flops" },
-  { word: "top notch", description: "the best" },
-  { word: "aggro", description: "aggressive" },
-  { word: "wanker", description: "a jerk" },
-  { word: 'You\'ve got Buckleys', description: 'you\'ve got no luck' }
+  {
+    word: 'cactus',
+    meaning: 'Dead.',
+  },
+  {
+    word: 'can\'t be stuffed',
+    meaning: 'Can\'t be messed with.',
+  },
+  {
+    word: 'chuck a block',
+    meaning: 'Its full.',
+  },
+  {
+    word: 'chuck a sikkie',
+    meaning: 'Call in sick.',
+  },
+  {
+    word: 'ciggy butt brain',
+    meaning: 'When you got ciggy butts all up in your brain.',
+    link: 'https://www.youtube.com/watch?v=vmL72sgVdAQ',
+  },
+  {
+    word: 'coasties',
+    meaning: 'Person living on coast.',
+  },
+  {
+    word: 'compo',
+    meaning: 'Compensation.',
+  },
+  {
+    word: 'connie, jonny',
+    meaning: 'Condom.',
+  },
+  {
+    word: 'cooee',
+    meaning: 'Something is close.',
+  },
+  {
+    word: 'cuppa',
+    meaning: 'Cup.',
+  },
+  {
+    word: 'daggy',
+    meaning: 'Dorky.',
+  },
+  {
+    word: 'dead set',
+    meaning: 'True.',
+  },
+  {
+    word: 'derro',
+    meaning: 'Derelict.',
+  },
+  {
+    word: 'different kettle of fish',
+    meaning: 'A different thing altogther.',
+  },
+  {
+    word: 'dinkie die',
+    meaning: 'Really true.',
+  },
+  {
+    word: 'docket',
+    meaning: 'Receipt.',
+  },
+  {
+    word: 'drongo',
+    meaning: 'Idiot.',
+  },
+  {
+    word: 'dunnie',
+    meaning: 'The loo.',
+  },
+  {
+    word: 'durries',
+    meaning: 'Already smoked cigarette/new cigarette.',
+  },
+  {
+    word: 'exxy',
+    meaning: 'Expensive.',
+  },
+  {
+    word: 'fair dinkum',
+    meaning: 'Genuine.',
+  },
+  {
+    word: 'galoot',
+    meaning: '',
+  },
+  {
+    word: 'get messed',
+    meaning: 'False off.',
+  },
+  {
+    word: 'gone walk-about',
+    meaning: 'It\'s lost.',
+  },
+  {
+    word: 'goon sack',
+    meaning: 'Swing it around.',
+  },
+  {
+    word: 'gridiron',
+    meaning: 'American football.',
+  },
+  {
+    word: 'grog',
+    meaning: 'Booze.',
+  },
+  {
+    word: 'he\'s loose',
+    meaning: 'He\'s drunk.',
+  },
+  {
+    word: 'he\'s off his face',
+    meaning: 'He\'s drunk.',
+  },
+  {
+    word: 'heaps',
+    meaning: 'A lot of, mate.',
+  },
+  {
+    word: 'hoolie doolie',
+    meaning: 'In surprised! Good heavens!',
+  },
+  {
+    word: 'hoon',
+    meaning: 'Mess around in car.',
+  },
+  {
+    word: 'jug',
+    meaning: 'Kettle or jug.',
+  },
+  {
+    word: 'jumbuck',
+    meaning: 'Sheep.',
+  },
+  {
+    word: 'jumper',
+    meaning: 'Hoodie.',
+  },
+  {
+    word: 'karked',
+    meaning: 'Something died.',
+  },
+  {
+    word: 'keen',
+    meaning: 'Down for it.',
+  },
+  {
+    word: 'larrikin',
+    meaning: 'Class clown.',
+  },
+  {
+    word: 'lift',
+    meaning: 'Elevator.',
+  },
+  {
+    word: 'lollies',
+    meaning: 'Candy.',
+  },
+  {
+    word: 'maggot',
+    meaning: 'Disgustingly drunk.',
+  },
+  {
+    word: 'mate',
+    meaning: 'A dude.',
+  },
+  {
+    word: 'mates rates',
+    meaning: 'Friend discount.',
+  },
+  {
+    word: 'mueslie',
+    meaning: 'Granola.',
+  },
+  {
+    word: 'onya',
+    meaning: 'Good on you.',
+  },
+  {
+    word: 'or nothing but buckleys',
+    meaning: 'You\'ve got no chance.',
+  },
+  {
+    word: 'petrol',
+    meaning: 'Gasoline.',
+  },
+  {
+    word: 'piece of piss',
+    meaning: 'It\'s so easy.',
+  },
+  {
+    word: 'postie',
+    meaning: 'Postman.',
+  },
+  {
+    word: 'prezzy',
+    meaning: 'Present.',
+  },
+  {
+    word: 'rego',
+    meaning: 'Vehicle registration.',
+  },
+  {
+    word: 'relo',
+    meaning: 'Relatives.',
+  },
+  {
+    word: 'ridgy-didge',
+    meaning: 'Something is good.',
+  },
+  {
+    word: 'rocket',
+    meaning: 'Arugula.',
+  },
+  {
+    word: 's\'avro',
+    meaning: 'This afternoon.',
+  },
+  {
+    word: 'saltana',
+    meaning: 'Raisin.',
+  },
+  {
+    word: 'schooner',
+    meaning: 'Measurement of beer.',
+  },
+  {
+    word: 'servo',
+    meaning: '',
+  },
+  {
+    word: 'shit all',
+    meaning: 'No chance.',
+  },
+  {
+    word: 'smoko',
+    meaning: 'A smoke break.',
+    link: 'https://www.youtube.com/watch?v=j58V2vC9EPc',
+  },
+  {
+    word: 'spicy',
+    meaning: 'Interesting, controversial and exciting.',
+  },
+  {
+    word: 'sticky beak',
+    meaning: 'Being nosy.',
+  },
+  {
+    word: 'straya',
+    meaning: 'Australia',
+  },
+  {
+    word: 'streuth',
+    meaning: 'Oh shit!.',
+  },
+  {
+    word: 'tactical vom',
+    meaning: 'Tactical vom.',
+  },
+  {
+    word: 'thongs',
+    meaning: 'Flip flops.',
+  },
+  {
+    word: 'tinny',
+    meaning: 'Can.',
+  },
+  {
+    word: 'tomato sauce',
+    meaning: 'Ketchup.',
+  },
+  {
+    word: 'top bloke',
+    meaning: 'Top bloke.',
+  },
+  {
+    word: 'top notch',
+    meaning: 'The best.',
+  },
+  {
+    word: 'true blue',
+    meaning: 'Really patriotic.',
+  },
+  {
+    word: 'vegemite',
+    meaning: 'Spread thin to avoid regret.',
+  },
+  {
+    word: 'wagga wagga',
+    meaning: 'Place in central.',
+  },
+  {
+    word: 'wanker',
+    meaning: 'A jerk.',
+  },
+  {
+    word: 'whinge',
+    meaning: 'Rant.',
+  },
+  {
+    word: 'whoop whoop',
+    meaning: 'Too far.',
+  },
+  {
+    word: 'yah nah / nah yah',
+    meaning: 'Yah nah / nah ya.',
+  },
+  {
+    word: 'you beaut',
+    meaning: 'Something is good.',
+  },
+  {
+    word: 'you\'ve got buckleys',
+    meaning: 'You\'ve got no luck.',
+  }
 ];
